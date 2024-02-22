d171a295ef9089497be43eb119d3d3b5ed418338
Dmitriy Dogonkin <dmitriydogonkin@mail.ru>
2024-02-23 01:24:07 +0500

    v2
    Working on a parser for posts.
    Problem: when a link to a post that contains only a video is entered, the parser starts taking the wrong src content

--- parser/parser.py
+++ parser/parser.py
@@ -4,16 +4,15 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-
 # Start chrome in background (headless mode)
 options = webdriver.ChromeOptions()
-options.add_argument('headless')
-options.add_argument('window-size=1920x935')
+# options.add_argument('headless')
+# options.add_argument('window-size=1920x935')
 driver = webdriver.Chrome(options=options)
 driver.get('about:blank')
 
 
-def reels(link):
+def reels(link: str):
     if link.startswith('https://www.instagram.com/') and 'reel' in link:
 
         # Open link
@@ -32,3 +31,41 @@ def reels(link):
 
     else:
         return None
+
+
+def post(link: str):
+    if link.startswith('https://www.instagram.com/') and '/p/' in link:
+        # Open link
+        driver.get(link)
+
+        # XPATH to element
+        photo_xpath = "//img[@class='x5yr21d xu96u03 x10l6tqk x13vifvy x87ps6o xh8yej3']"
+        button_xpath = "//button[@aria-label='Далее']"
+
+        # Wait for Instagram
+        WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, photo_xpath)))
+
+        unique_links = []
+        try:
+            button_element = driver.find_element(By.XPATH, button_xpath)
+            for i in range(4):
+                button_element.click()
+        except Exception:
+            pass
+        elements = driver.find_elements(By.XPATH, photo_xpath)
+        for photo in elements:
+            unique_links.append(photo.get_attribute('src'))
+        return unique_links
+
+    else:
+        return None
+
+
+# link = ""
+# result_generator = post(link)
+# print(len(result_generator))
+# print(result_generator)
+#
+# # for photo_src in result_generator:
+# #     print(photo_src)
+# # print(post(link))
